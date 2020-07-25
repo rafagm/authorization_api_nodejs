71b2cd4a474594f54ad0508ffa44b2578f03248c
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-07-25 23:59:52 +0200

    Refactor: A little refactor on authentication end points
    Add: Swagger documentation

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+
+//Set up Swagger
+const swaggerDocument = YAML.load('./routes/swagger/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Import Routes
 const authRoutes = require('./routes/auth');
--- routes/auth.js
+++ routes/auth.js
@@ -28,14 +28,14 @@ router.post('/register', async (req, res) => {
 
 
     const userWithExistingEmail = await User.findOne({ email: newUser.email });
-    if (userWithExistingEmail) return res.status(400).send("The email already exists");
+    if (userWithExistingEmail) return res.status(409).send("The email already exists");
 
     
     try {
         const savedUser = await newUser.save();
         res.send({userId: savedUser._id});
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
     }
 });
 
@@ -48,12 +48,13 @@ router.post('/login', async (req, res) => {
     }
 
     const vagueResponseMessage = "The email or password is wrong";
+    const vagueReponse = { error: vagueResponseMessage };
 
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send(vagueResponseMessage);
+    if (!user) return res.status(400).send(vagueReponse);
 
     const passwordsMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!passwordsMatch) return res.status(400).send(vagueResponseMessage);
+    if (!passwordsMatch) return res.status(400).send(vagueReponse);
 
     //Create and assing token
     const token = jwt.sign({id: user._id}, process.env.TOKEN_SECRET, {expiresIn: 15});
